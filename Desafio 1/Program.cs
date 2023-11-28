16d080250e2ebb3bb3910deb0112e845791a0b3f
diazdante <ddantediazz@gmail.com>
2023-11-28 16:40:32 -0300

    Ejercicio 1

---
+++ Desafio 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Telefono telefono1 = new Telefono("J7","Samsung");
+          
+
+            telefono1.NumeroTelefonico = "1151639389";
+            telefono1.CodigoOperador = 1;
+
+            Console.WriteLine("Tengo un " + telefono1.Marca + " modelo " + telefono1.Modelo);
+            Console.WriteLine("Mi numero de telefono es " + telefono1.NumeroTelefonico + " y el codigo de operador es " + telefono1.CodigoOperador);
+
+
+            Console.WriteLine(telefono1.llamar());
+            Console.WriteLine(telefono1.llamar("dante"));
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Desafio 1/Telefono.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_1
+{
+    internal class Telefono
+    {
+        //ATRIBUTOS
+        private string modelo;
+        private string marca;
+        private string numeroTelefonico;
+        private int codigoOperador;
+
+        //PROPIEDADES
+        public string Modelo { get; }
+        public string Marca { get; }
+        public string NumeroTelefonico
+        {
+            get { return numeroTelefonico; }
+            set { numeroTelefonico = value; }
+        }
+        public int CodigoOperador
+        {
+            get { return codigoOperador; }
+            set
+            {
+                if (codigoOperador == 1 || codigoOperador == 2 || codigoOperador == 3)
+                {
+                    codigoOperador = value;
+                }
+                else
+                {
+                    codigoOperador = 0;
+                }
+            }
+        }
+
+        //CONSTRUCTOR
+        public Telefono(string modelo, string marca)
+        {
+            Modelo = modelo;
+            Marca = marca;
+        }
+
+        //METODO
+
+        public string llamar()
+        {
+            return "Realizando llamada..";
+        }
+        //SOBRECARGA
+        public string llamar(string contacto)
+        {
+            return "Realizando llamada.. " + contacto;
+        }
+
+    }
+}
