0bda5d04f5c26043987875041598a6be2628fbea
“Dante” <ddantediazz@gmail.com>
2023-11-28 16:29:19 -0300

    Constructores y Destructores

---
+++ Constructores y destructores/Botella.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructores_y_destructores
+{
+    internal class Botella
+    {
+        //Constructor
+
+        public Botella(string color, string material)
+        {
+            this.color = color; //this es un puntero que hace referencia a la clase
+            this.Material = material;
+        }
+
+        //Sobrecarga constructor (crear mismo constructor pero con otros parametros)
+
+        public Botella() { }
+
+        //Botella: capacidad, color, material.
+        private int capacidad;
+        private string color;
+        public string Material { get; set; }
+
+        public int Capacidad
+        {
+            get { return capacidad; }
+            set { capacidad = value; }
+        }
+    }
+}
---
+++ Constructores y destructores/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructores_y_destructores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Botella b1 = new Botella("Rojo", "Plastico");
+            Botella b2 = new Botella();
+        }
+    }
+}
